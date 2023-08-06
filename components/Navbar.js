02326ca3aa62b936bce08fca1929c4cefa1e459e
Ryoto Ogawa <71643807+ryotoitoi@users.noreply.github.com>
2023-08-06 10:56:05 +0000

    update ui

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,18 @@
 import Link from 'next/link';
+import styles from '../styles/navbar.module.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>ホーム</a>
-          </Link>
+    <nav className={styles.container}>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <Link href="/">ホーム</Link>
         </li>
-        <li>
-          <Link href="/about">
-            <a>私について</a>
-          </Link>
+        <li className={styles.li}>
+          <Link href="/about">私について</Link>
         </li>
-        <li>
-          <Link href="/projects">
-            <a>プロジェクト</a>
-          </Link>
+        <li className={styles.li}>
+          <Link href="/projects">プロジェクト</Link>
         </li>
       </ul>
     </nav>
